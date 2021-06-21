643126a8f7a727fd70cf1e1af0c7559849c5501f
nathan hains <nathanhains14@yahoo.com>
2021-06-21 17:35:13 -0400

    Seed data for all current models.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# User.create(name: "nate", avatar: "undefined", username: "natehains", email: "natehains@yahoo.com", password: "password")
-# Category.create(name: "Back")
+n = User.create(name: "nate", avatar: "undefined", username: "natehains", email: "natehains@yahoo.com", password: "password")
+Category.create(name: "Back")
+n.exercises.create(name: "Pull-up", instructions: "1. Pull your body up.", category_id: 1)
+n.workouts.create(name: "Monday Routine", notes: "8:00am, twice.")
+n.workouts.first.workout_exercises.create(exercise_id: 1)
+n.workouts.first.workout_exercises.first.set_groups.create(lbs: 170, reps: 12)
+
+
+
 
