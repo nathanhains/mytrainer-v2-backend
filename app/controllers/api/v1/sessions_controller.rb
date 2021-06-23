29dc0afa0af157245170bd0cc816f8fba9183a94
nathan hains <nathanhains14@yahoo.com>
2021-06-23 11:01:08 -0400

    User signup functionality.

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::SessionsController < ApplicationController
         @user = User.find_by(username: params[:session][:username])
         if @user && @user.authenticate(params[:session][:password])
             session[:user_id] = @user.id
-            render json: @user
+            render json: UserSerializer.new(@user), status: :ok
         else
             render json: {
                 error: "Invalid Credentials"
@@ -13,7 +13,7 @@ class Api::V1::SessionsController < ApplicationController
 
     def get_current_user
         if logged_in? 
-            render json: current_user
+            render json: UserSerializer.new(current_user)
         else
             render json: {
                 error: "Log in required"
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
     has_secure_password
     has_many :exercises
     has_many :workouts
+
+    validates :name, :username, :email, presence: true
+    validates :username, :email, uniqueness: true
 end
